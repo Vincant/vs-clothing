6147237be2edcf31a9f20e37fd6529923b156b0a
Volodymyr Suskyi <volodymyr.suskyi@gmail.com>
2020-01-29 19:47:23 +0200

    Updates

---
+++ precache-manifest.49cb214048bef28c129b8576b739c176.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a01760a4c75bf3ec76a89896bbdb1328",
+    "url": "/vs-clothing/index.html"
+  },
+  {
+    "revision": "f623ccbd7832a1b33dc6",
+    "url": "/vs-clothing/static/css/main.e99c746e.chunk.css"
+  },
+  {
+    "revision": "13b6cc17eee643c20376",
+    "url": "/vs-clothing/static/js/2.b71b94f6.chunk.js"
+  },
+  {
+    "revision": "f623ccbd7832a1b33dc6",
+    "url": "/vs-clothing/static/js/main.6a556820.chunk.js"
+  },
+  {
+    "revision": "060d6cafa7e22c02eb63",
+    "url": "/vs-clothing/static/js/runtime~main.bdc3399a.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/vs-clothing/static/media/logo.19ef7b17.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/vs-clothing/static/media/shopping-bag.30f07071.svg"
+  }
+]);
